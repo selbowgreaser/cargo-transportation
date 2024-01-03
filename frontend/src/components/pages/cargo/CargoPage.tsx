7c4af56b7bd685e39096e16b78f60a81f462008b
leonid <kotovpetia@gmail.com>
2024-01-04 01:24:14 +0300

    #17 add sorting

--- frontend/src/components/pages/cargo/CargoPage.tsx
+++ frontend/src/components/pages/cargo/CargoPage.tsx
@@ -7,32 +7,68 @@ import {useRequest} from "../../../hooks/useRequest";
 
 const CargoPage = () => {
     const [cargoList, setCargoList] = useState<Cargo[]>([])
-    const [filteredCargoList, setFilteredCargoList] = useState<Cargo[]>([])
+    const [filterPredicate, setFilterPredicate] = useState(() => (cargo: Cargo) => true)
+    const [comparator, setComparator] = useState(() => (a: Cargo, b: Cargo) => a.id - b.id)
+    const [sortedAndFilteredCargoList, setSortedAndFilteredCargoList] = useState<Cargo[]>([])
 
-    const [fetchCargoList, isCargoListLoading, fetchCargoListError] = useRequest(async () => {
+    const [fetchCargoList, isCargoListLoading, error] = useRequest(async () => {
         const cargoList = await CargoApiClient.findAllCargo()
         setCargoList([...cargoList])
-        setFilteredCargoList([...cargoList])
+        setSortedAndFilteredCargoList([...cargoList])
     });
 
     useEffect(() => {
         fetchCargoList()
     }, [])
 
+    const setCargo = (cargo: Cargo) => {
+        const index = cargoList.findIndex(it => it.id === cargo.id);
+        const updatedCargoList = [
+            ...cargoList.slice(0, index),
+            cargo,
+            ...cargoList.slice(index + 1)
+        ];
+        setCargoList(updatedCargoList);
+    }
+
+    const removeCargo = (cargo: Cargo) => {
+        setCargoList(cargoList.filter(it => it.id !== cargo.id))
+    }
+
+    const filterCargoList = () => {
+        const filteredCargoList = cargoList.filter(filterPredicate)
+        setSortedAndFilteredCargoList([...filteredCargoList])
+    }
+
+    const sortCargoList = () => {
+        const sortedCargoList = [...cargoList].sort(comparator)
+        setSortedAndFilteredCargoList([...sortedCargoList])
+    }
+
+    useEffect(() => {
+        filterCargoList()
+    }, [cargoList, filterPredicate])
+
+    useEffect(() => {
+        sortCargoList()
+    }, [cargoList, comparator])
+
     return (
         <div className="flex justify-center h-screen my-24">
             <div className="w-2/3">
                 <CargoSelectorBar
                     cargoList={cargoList}
-                    setFilteredCargoList={setFilteredCargoList}
+                    setFilterPredicate={setFilterPredicate}
                 />
                 {isCargoListLoading ?
                     <div>Loading</div>
                     :
                     <CargoItemList
-                        filteredCargoList={filteredCargoList}
-                        cargoList={cargoList}
-                        setCargoList={setCargoList}
+                        sortedAndFilteredCargoList={sortedAndFilteredCargoList}
+                        setCargo={setCargo}
+                        removeCargo={removeCargo}
+                        setComparator={setComparator}
+                        filterPredicate={filterPredicate}
                     />
                 }
             </div>
