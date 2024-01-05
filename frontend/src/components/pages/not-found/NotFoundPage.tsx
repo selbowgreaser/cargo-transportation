87b1fe2d725d149b0b102382e8eed24fdc5b09e9
leonid <kotovpetia@gmail.com>
2024-01-05 20:07:43 +0300

    #26 update NavigationBar: add posts page button and username

--- frontend/src/components/pages/not-found/NotFoundPage.tsx
+++ frontend/src/components/pages/not-found/NotFoundPage.tsx
@@ -1,12 +1,12 @@
 import {Link} from "react-router-dom";
 
-const   NotFoundPage = () => {
+const NotFoundPage = () => {
     return <div className="min-h-screen flex flex-grow items-center justify-center bg-gray-50">
         <div className="p-8 text-center">
             <h1 className="mb-4 text-4xl font-bold">404</h1>
             <p className="text-gray-600">Запрашиваемая страница не найдена</p>
             <Link to="/"
-               className="mt-4 inline-block rounded bg-blue-500 px-4 py-2 font-semibold text-white hover:bg-blue-600">
+                  className="mt-4 inline-block rounded bg-blue-600 px-4 py-2 font-semibold text-white hover:opacity-90">
                 Вернуться на главную
             </Link>
         </div>
--- frontend/src/services/AuthService.ts
+++ frontend/src/services/AuthService.ts
@@ -2,6 +2,17 @@ import AuthApiClient from "../api/AuthApiClient";
 import {SignUpFormData} from "../components/models/SignUpFormData";
 import {SignInFormData} from "../components/models/SignInFormData";
 
+class LocalStorageKeys {
+
+    static get userAuthHeaderKey() {
+        return "cargo-express-user-auth";
+    }
+
+    static get usernameKey() {
+        return "cargo-express-username";
+    }
+}
+
 export default class AuthService {
 
     static async register(formData: SignUpFormData) {
@@ -15,7 +26,8 @@ export default class AuthService {
         const response = await AuthApiClient.login(user)
 
         if (response["jwt-token"]) {
-            localStorage.setItem("userAuth", `Bearer ${response["jwt-token"]}`);
+            localStorage.setItem(LocalStorageKeys.userAuthHeaderKey, `Bearer ${response["jwt-token"]}`);
+            localStorage.setItem(LocalStorageKeys.usernameKey, formData.username);
         }
     }
 
@@ -26,21 +38,27 @@ export default class AuthService {
         })
 
         if (response["jwt-token"]) {
-            localStorage.setItem("userAuth", `Bearer ${response["jwt-token"]}`);
+            localStorage.setItem(LocalStorageKeys.userAuthHeaderKey, `Bearer ${response["jwt-token"]}`);
+            localStorage.setItem(LocalStorageKeys.usernameKey, formData.username);
             return true
         }
         return false
     }
 
+    static username() {
+        return localStorage.getItem(LocalStorageKeys.usernameKey);
+    }
+
     static logout() {
-        localStorage.removeItem("userAuth");
+        localStorage.removeItem(LocalStorageKeys.userAuthHeaderKey);
+        localStorage.removeItem(LocalStorageKeys.usernameKey);
     }
 
     static isAuth() {
-        return localStorage.getItem("userAuth") !== null
+        return localStorage.getItem(LocalStorageKeys.userAuthHeaderKey) !== null
     }
 
     static authHeader() {
-        return localStorage.getItem("userAuth")
+        return localStorage.getItem(LocalStorageKeys.userAuthHeaderKey)
     }
 }
